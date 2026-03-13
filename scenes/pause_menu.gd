0aa5bf97beac3ca078b43d4a912d2d13cfb775c5
Carmen <54728377+crfews@users.noreply.github.com>
2026-03-13 15:43:18 -0400

    added pause menu node and script

---
+++ scenes/pause_menu.gd
@@ -0,0 +1,39 @@
+extends Control
+
+func resume():
+	get_tree().paused = false
+	$AnimationPlayer.play_backwards("pause_blur")
+	
+func pause():
+	get_tree().paused = true;
+	$AnimationPlayer.play("pause_blur")
+	
+
+func test_esc():
+	if Input.is_action_just_pressed("esc") and get_tree().paused == false:
+		pause()
+	elif Input.is_action_just_pressed("esc") and get_tree().paused == true:
+		resume()
+		
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$AnimationPlayer.play("RESET")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	test_esc()
+
+
+func _on_resume_pressed() -> void:
+	resume()
+
+
+func _on_restart_pressed() -> void:
+	resume()
+	get_tree().reload_current_scene()
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()

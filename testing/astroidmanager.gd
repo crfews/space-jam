8ef88dd1d98fd9b120b2f41915c6269e6f9e52bd
ShawnPatrick-Barhorst <shawnpatrick.barhorst@gmail.com>
2026-03-14 02:33:46 -0400

    Updates to Kendrick's implimentation )

--- testing/astroidmanager.gd
+++ testing/astroidmanager.gd
@@ -1,55 +1,61 @@
 extends Node3D
 
 @export var asteroid_scene: PackedScene
+@export var player : Node3D
+@export var num_asteroids := 500
+@export var field_radius := 100.0
+@export var asteroid_speed := 50.0
+@export var n_layers := 5
+@export var layer_spacing := 20.0
+@export var remove_y := 50.0
 
-@export var num_asteroids_per_slice: int = 120
-@export var field_radius: float = 35.0
-@export var asteroid_speed: float = 15.0
-@export var n_layers: int = 6
-@export var layer_spacing: float = 18.0
-@export var remove_y: float = 10.0
-@export var player: CharacterBody3D
-
-var rendered_slices: Array[Node3D] = []
+var _rendered_slices: Array[Node3D] = []
 
 func _ready() -> void:
 	randomize()
 
+	# Build all slices once
 	for i in range(n_layers):
 		var slice := create_slice()
 		slice.position.y = -i * layer_spacing
 		add_child(slice)
-		rendered_slices.append(slice)
+		_rendered_slices.append(slice)
+
 
 func _process(delta: float) -> void:
-	for slice in rendered_slices:
+	# Move whole slices, not individual asteroids
+	for slice in _rendered_slices:
 		slice.position.y += asteroid_speed * delta
 
-	if rendered_slices.is_empty():
+	if _rendered_slices.is_empty():
 		return
 
-	var front_slice := rendered_slices[0]
+	var front_slice := _rendered_slices[0]
 
+	# Recycle front slice to the back
 	if front_slice.position.y > remove_y:
-		rendered_slices.pop_front()
+		_rendered_slices.pop_front()
 
 		var back_y := get_backmost_y()
+
 		front_slice.position.y = back_y - layer_spacing
-		front_slice.position.x = player.global_position.x
-		front_slice.position.z = player.global_position.z
+		front_slice.position.x = player.position.x  # Keep slices centered on player
+		front_slice.position.z = player.position.z
 		randomize_slice(front_slice)
 
-		rendered_slices.append(front_slice)
+		_rendered_slices.append(front_slice)
+
 
 func create_slice() -> Node3D:
 	var slice := Node3D.new()
 
-	for i in range(num_asteroids_per_slice):
-		var asteroid = asteroid_scene.instantiate()
+	for i in range(num_asteroids):
+		var asteroid := asteroid_scene.instantiate() as Node3D
 		slice.add_child(asteroid)
 
 	randomize_slice(slice)
 	return slice
+
 
 func randomize_slice(slice: Node3D) -> void:
 	for asteroid in slice.get_children():
@@ -68,14 +74,15 @@ func randomize_slice(slice: Node3D) -> void:
 			randf_range(0.0, TAU)
 		)
 
-		var random_scale := randf_range(0.7, 1.4)
-		asteroid.scale = Vector3.ONE * random_scale
+		asteroid.scale = Vector3.ONE * randf_range(0.7, 1.4)
+
 
 func get_backmost_y() -> float:
-	var min_y := rendered_slices[0].position.y
+	var min_y := _rendered_slices[0].position.y
 
-	for slice in rendered_slices:
+	for slice in _rendered_slices:
 		if slice.position.y < min_y:
 			min_y = slice.position.y
 
 	return min_y
+

dc24883c0b9f578e58a9a50a6d14d4d3e241cfcc
ShawnPatrick-Barhorst <shawnpatrick.barhorst@gmail.com>
2026-03-13 21:58:09 -0400

    asteroid field optimizations

--- generate_asteroid_field.gd
+++ generate_asteroid_field.gd
@@ -1,80 +1,84 @@
 extends Node3D
 
 @export var asteroid_scene: PackedScene
-@export var num_asteroids := 50
-@export var field_radius := 50.0
-@export var asteroid_speed := 10.0
+@export var num_asteroids := 500
+@export var field_radius := 100.0
+@export var asteroid_speed := 30.0
 @export var n_layers := 5
 @export var layer_spacing := 20.0
+@export var remove_y := 50.0
 
-var _asteroid_slices = []
+var _rendered_slices: Array[Node3D] = []
 
-func generate_asteroid_slice() -> Array[MeshInstance3D]:
-	# Generates a 2D slice of asteroids in the XZ plane.
-	var asteroids: Array[MeshInstance3D] = []
+func _ready() -> void:
+	randomize()
+
+	# Build all slices once
+	for i in range(n_layers):
+		var slice := create_slice()
+		slice.position.y = -i * layer_spacing
+		add_child(slice)
+		_rendered_slices.append(slice)
+
+
+func _process(delta: float) -> void:
+	# Move whole slices, not individual asteroids
+	for slice in _rendered_slices:
+		slice.position.y += asteroid_speed * delta
+
+	if _rendered_slices.is_empty():
+		return
+
+	var front_slice := _rendered_slices[0]
+
+	# Recycle front slice to the back
+	if front_slice.position.y > remove_y:
+		_rendered_slices.pop_front()
+
+		var back_y := get_backmost_y()
+
+		front_slice.position.y = back_y - layer_spacing
+		randomize_slice(front_slice)
+
+		_rendered_slices.append(front_slice)
+
+
+func create_slice() -> Node3D:
+	var slice := Node3D.new()
+
 	for i in range(num_asteroids):
-		var asteroid_instance := asteroid_scene.instantiate() as MeshInstance3D
+		var asteroid := asteroid_scene.instantiate() as Node3D
+		slice.add_child(asteroid)
+
+	randomize_slice(slice)
+	return slice
+
+
+func randomize_slice(slice: Node3D) -> void:
+	for asteroid in slice.get_children():
 		var angle := randf() * TAU
 		var distance := sqrt(randf()) * field_radius
-		asteroid_instance.position = Vector3(cos(angle) * distance, 0, sin(angle) * distance)
-		asteroids.append(asteroid_instance)
-	return asteroids
+
+		asteroid.position = Vector3(
+			cos(angle) * distance,
+			0.0,
+			sin(angle) * distance
+		)
+
+		asteroid.rotation = Vector3(
+			randf_range(0.0, TAU),
+			randf_range(0.0, TAU),
+			randf_range(0.0, TAU)
+		)
+
+		asteroid.scale = Vector3.ONE * randf_range(0.7, 1.4)
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	# Generate n layers of asteroids with increasing distance from the camera
+func get_backmost_y() -> float:
+	var min_y := _rendered_slices[0].position.y
 
-	for i in range(n_layers):
-		var slice := generate_asteroid_slice()
-		for asteroid in slice:
-			asteroid.position.y = -i * layer_spacing
-			add_child(asteroid)
-		#_asteroid_slices.append(slice)
-		_asteroid_slices.push_front(slice)
+	for slice in _rendered_slices:
+		if slice.position.y < min_y:
+			min_y = slice.position.y
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	#move towards camera
-	for asteroid in get_children():
-		if asteroid is MeshInstance3D:
-			asteroid.translate(Vector3(0, 10 * delta, 0))
-
-	#remove asteroids that have passed the camera
-	for asteroid in get_children():
-
-		# Free individual 
-		# if asteroid is MeshInstance3D and asteroid.position.y > 50:
-		# 	asteroid.queue_free()
-
-		# Free entire slice if any asteroid in it has passed the camera
-		for slice in _asteroid_slices:
-			if slice.size() > 0 and slice[0].position.y > 50:
-				for a in slice:
-					a.queue_free()
-				_asteroid_slices.erase(slice)
-
-
-	# If asteroid is removed, add a new one at the back of the field
-	# if _asteroid_slices.size() < n_layers:
-	# 	var slice := generate_asteroid_slice()
-	# 	for asteroid in slice:
-	# 		asteroid.position.y = -n_layers * layer_spacing
-	# 		add_child(asteroid)
-	# 	_asteroid_slices.append(slice)
-
-	# If asteroid slice is removed, add a new one at the layer_spacing behind the last layer
-	if _asteroid_slices.size() < n_layers:
-		var slice := generate_asteroid_slice()
-
-		var last_layer_y: float = _asteroid_slices[0][0].position.y
-		var new_layer_y: float = last_layer_y - layer_spacing
-
-		for asteroid in slice:
-			asteroid.position.y = new_layer_y
-			add_child(asteroid)
-
-		_asteroid_slices.push_front(slice)
-	
-	pass
+	return min_y
